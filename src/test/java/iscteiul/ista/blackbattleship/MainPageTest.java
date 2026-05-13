f8ca832cf584940db38a7e53b6ad6eac85e7c0eb
Margarida Ribeiro <meroo@iscte-iul.pt>
2026-05-13 21:41:53 +0100

    Tentei corrigir os testes atualizando os seletores e tratando o popup de cookies. O browser abre corretamente e executa as ações na página, mas alguns testes continuam a falhar porque o site da JetBrains mudou desde que o código foi criado.

--- src/test/java/iscteiul/ista/blackbattleship/MainPageTest.java
+++ src/test/java/iscteiul/ista/blackbattleship/MainPageTest.java
@@ -1,55 +1,61 @@
+// MainPageTest.java
 package iscteiul.ista.blackbattleship;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import static com.codeborne.selenide.Condition.attribute;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainPageTest {
+
     MainPage mainPage = new MainPage();
 
     @BeforeAll
     public static void setUpAll() {
         Configuration.browserSize = "1280x800";
+        Configuration.timeout = 10000;
         SelenideLogger.addListener("allure", new AllureSelenide());
     }
 
     @BeforeEach
     public void setUp() {
         open("https://www.jetbrains.com/");
+
+        if ($("div.ch2-container").exists()) {
+            $$("button").findBy(exactText("Accept All")).click();
+            $("div.ch2-container").should(disappear);
+        }
     }
 
     @Test
     public void search() {
-        mainPage.searchButton.click();
+        mainPage.searchButton.shouldBe(visible).click();
 
-        $("[data-test='search-input']").sendKeys("Selenium");
-        $("button[data-test='full-search-button']").click();
+        $("[data-test='search-input']").shouldBe(visible).sendKeys("Selenium");
+        $("button[data-test='full-search-button']").shouldBe(visible).click();
 
         $("input[data-test='search-input']").shouldHave(attribute("value", "Selenium"));
     }
 
     @Test
     public void toolsMenu() {
-        mainPage.toolsMenu.click();
-
+        mainPage.toolsMenu.shouldBe(visible).click();
         $("div[data-test='main-submenu']").shouldBe(visible);
     }
 
     @Test
     public void navigationToAllTools() {
-        mainPage.seeDeveloperToolsButton.click();
-        mainPage.findYourToolsButton.click();
+        mainPage.seeDeveloperToolsButton.shouldBe(visible).click();
+        mainPage.findYourToolsButton.shouldBe(visible).click();
 
         $("#products-page").shouldBe(visible);
-
         assertEquals("All Developer Tools and Products by JetBrains", Selenide.title());
     }
 }
